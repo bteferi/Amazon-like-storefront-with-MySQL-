d977371fe97d6e810635a3430cb4dd4705a23239
Beza <bezawit.teferi@gmail.com>
2018-02-19 14:40:24 -0500

    mysql is successfully conntected  and we got res object back :)

---
+++ bamazon.sql
@@ -0,0 +1,53 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name  VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price   INT(100) NOT NULL,
+  stock_quantity INT(100) NOT NULL,
+  PRIMARY KEY (item_id)
+
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Iphone X", "electronics", "200", "12");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("PlayStation", "electronics", "100", "5");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Lipitor", "pharmaceutical", "50", "10");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Drone", "electronics", "300", "8");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("star Wars", "movies", "15", "50");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Harry Potter", "book", "25", "25");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("iPad", "tablet", "75", "10");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("iPhone", "smartphone", "320", "7");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Coconut oil", "cosmetics", "25", "75");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Coconut oil", "cosmetics", "25", "75");
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,42 @@
 var inquirer = require("inquirer")
-var mySQL = require("mysql")
+var mysql = require("mysql")
   require("dotenv").config();
 
 
+//
+// console.log(process.env.DB_HOST);
+//
+// console.log(process.env.DB_PORT)
+//
+// console.log(process.env.DB_USER);
+//
+// console.log(process.env.DB_PASSW)
 
-console.log(process.env.DB_HOST);
 
-console.log(process.env.DB_PORT)
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 
-console.log(process.env.DB_USER);
+  // Your username
+  user: process.env.DB_USER,
 
-console.log(process.env.DB_PASSW)
+  // Your password
+  password: process.env.DB_PASSW,
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // SAVE THE RESPONCE AS A VAR
+    console.log(res[0]);
+    console.log(res[0].price); // how to parse it 
+    connection.end();
+  });
+}
