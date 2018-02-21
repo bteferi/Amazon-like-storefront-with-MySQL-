18a0e00b85c93d6dce809bbf58d922abbea69765
Beza <bezawit.teferi@gmail.com>
2018-02-20 19:55:09 -0500

    redoing function to grab user entered data to update database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer")
 var mysql = require("mysql")
 require("dotenv").config();
+
 //====================================================================//
 
 var connection = mysql.createConnection({
@@ -18,74 +19,112 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  // console.log("connected as id " + connection.threadId);
-  currentProducts();
+  console.log(" *****************HERE IS EVERTHING IN STORE*******************");
+  everythingInStore();
+   //
 });
 //===========================BELOW IS WHERE i CAN ADD MY CODE TO PRING ==/
-function currentProducts() {
+
+function everythingInStore() {
  connection.query('SELECT * FROM products', function(error, res) {
     if (error) throw err;
     //======================no error go to the next step ===
     // console.log(res)
     res.forEach(function(res) {
         // console.log(res);
-      // console.log("Item Id :" +  res.item_id  + " , name : " + res.product_name + " , Dept : " +  res.department_name + ", Price" +  res.price);
+      console.log("Item Id :" +  res.item_id  + " , name : " + res.product_name + " , Dept : " +  res.department_name + ", Price" +  res.price + "\n");
+    console.log ("-----------------------------------------------------------------------------------");
+
+
     });
       connection.end();
 });
 }
+
+
+
+
 //================== question for customers ==//
 
+// selectProductId();
+function selectProductId(){
+  inquirer
+    .prompt([
 
-inquirer
-  .prompt([
-    // Here we create a basic text prompt.
-    {
-      type: "confirm",
-      message: "welcome to Bamazon! go shopping?",
-      name: "confirm",
-      default:true
-       //====what if customer doesnt wana shop
-    },
-    // if user hits Y then continue to show what is in store
-    {
-      type: "list",
-      message: "Below is what is currently available in store. Please select the product you would like to purchase at this moment",
-      choices: ["Iphone X", "PlayStation", "Lipitor", "Drone","Star Wars","Harry Potter","iPad","Coconut oil","Green Tea"],
-      name: "banazonProducts"
-      // save the item the user picks in a var so we can show it in the below code
-    },
-    // Here products being purchased - quantity of products.
-    // later on the promise stage convert this inpur to parsInt as "inpur" has a string value
-    {
-      type: "input",
-      message: "How many of this product would you like to purchase?", // add a var for this - ones the user pick from the above list save it to a variable then disolay it here
-      name: "productQuantity"
-      // save this quantity to a var and it must be in parsInt
-      // we have this much ..how many would u like to buy?
-      // save the inital value of this product and somwhat show it here
-    },
-    {
-      type: "confirm",
-      message: "You are purchasing this much product for this much money :",
-      name: "confirmPurchase",
-      default: true
-      // again we need that var user above to say u r buying this much please confitm
-      // purchasing this much (product name) for this ( cost amount)
-    },
-    // Here we give the user a list to choose from.
-    {
-      type: "confirm",
-      message: "Thank you for shopping at Bamazone would u like to do another purchase?:",
-      name: "confirm",
-      default: true
-      // ?  bc I would guess were here to purchase just one Item
-        // if user hits yes restart the game ..if user says no - end game
-    },
+      {
+        type: "input",
+        message: "Please select the Product ID for the Item you would like to purchase",
+        name: "PickProductID",
 
-  ]).then(function(inquirerResponse) {
-        // here goes all my response ???
-          if (inquirerResponse.confirm) {
-          console.log("\nWelcome " + inquirerResponse.banazonProducts)
-        }
-  });
+      },
+]).then(function(inquirerResponse) {
+      // here goes all my response
+        if (inquirerResponse.PickProductID) {
+        console.log(" test")
+
+        //grab all the cutomer response
+      }
+})
+
+};
+
+
+
+
+
+
+
+
+// might need to mute this part
+// inquirer
+//   .prompt([
+//     // Here we create a basic text prompt.
+//     {
+//       type: "confirm",
+//       message: "welcome to Bamazon! go shopping?",
+//       name: "confirm",
+//       default:true
+//        //====what if customer doesnt wana shop
+//     },
+//     // if user hits Y then continue to show what is in store
+//     {
+//       type: "list",
+//       message: "Below is what is currently available in store. Please select the product you would like to purchase at this moment",
+//       choices: ["Iphone X", "PlayStation", "Lipitor", "Drone","Star Wars","Harry Potter","iPad","Coconut oil","Green Tea"],
+//       name: "banazonProducts"
+//       // save the item the user picks in a var so we can show it in the below code
+//     },
+//     // Here products being purchased - quantity of products.
+//     // later on the promise stage convert this inpur to parsInt as "inpur" has a string value
+//     {
+//       type: "input",
+//       message: "How many of this product would you like to purchase?", // add a var for this - ones the user pick from the above list save it to a variable then disolay it here
+//       name: "productQuantity"
+//       // save this quantity to a var and it must be in parsInt
+//       // we have this much ..how many would u like to buy?
+//       // save the inital value of this product and somwhat show it here
+//     },
+//     {
+//       type: "confirm",
+//       message: "You are purchasing this much product for this much money :",
+//       name: "confirmPurchase",
+//       default: true
+//       // again we need that var user above to say u r buying this much please confitm
+//       // purchasing this much (product name) for this ( cost amount)
+//     },
+//     // Here we give the user a list to choose from.
+//     {
+//       type: "confirm",
+//       message: "Thank you for shopping at Bamazone would u like to do another purchase?:",
+//       name: "confirm",
+//       default: true
+//       // ?  bc I would guess were here to purchase just one Item
+//         // if user hits yes restart the game ..if user says no - end game
+//     },
+//
+//   ]).then(function(inquirerResponse) {
+//         // here goes all my response ???
+//           if (inquirerResponse.confirm) {
+//           // console.log("\nWelcome " + inquirerResponse.banazonProducts)
+//         }
+//   });
